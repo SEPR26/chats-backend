dea9e9420617e4789f0d715451b898dd4081d5e5
sepr26 <rasulsaparbaev@gmail.com>
2023-06-13 16:29:20 +0500

    fixed ports in redis

--- src/config.ts
+++ src/config.ts
@@ -20,12 +20,12 @@ class Config {
 
   constructor() {
     this.DATABASE_URL = process.env.DATABASE_URL || this.DEFAULT_DATABASE_URL;
-    this.JWT_TOKEN = process.env.DATABASE_URL || '1234';
-    this.NODE_ENV = process.env.DATABASE_URL || '';
-    this.SECRET_KEY_ONE = process.env.DATABASE_URL || '';
-    this.SECRET_KEY_TWO = process.env.DATABASE_URL || '';
-    this.CLIENT_URL = process.env.DATABASE_URL || '';
-    this.REDIS_HOST = process.env.DATABASE_URL || '';
+    this.JWT_TOKEN = process.env.JWT_TOKEN || '1234';
+    this.NODE_ENV = process.env.NODE_ENV || '';
+    this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || '';
+    this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || '';
+    this.CLIENT_URL = process.env.CLIENT_URL || '';
+    this.REDIS_HOST = process.env.REDIS_HOST || '';
     this.CLOUD_NAME = process.env.CLOUD_NAME || '';
     this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || '';
     this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || '';
